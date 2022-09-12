63a7f9174df13812fc6cc3c4501adfa5aaf4b530
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-09-12 16:19:00 +0300

    add webpack

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { Component } from './base-cmp.js'
-import * as Validation from '../util/validation.js'
-import { autobind as Autobind } from '../decorators/autobind.js'
-import { projectState } from '../state/project-state.js'
+import { Component } from './base-cmp'
+import * as Validation from '../util/validation'
+import { autobind as Autobind } from '../decorators/autobind'
+import { projectState } from '../state/project-state'
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
